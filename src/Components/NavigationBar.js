4714eaa6e9922dcf22a6260f49b8556b2ccad2b7
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-06-30 19:20:26 +0700

    create navbar

---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navbar, Container, Nav, Button } from 'react-bootstrap'
+
+export default function NavigationBar() {
+    return (
+        <Navbar expand="lg" className='shadow py-3' fixed="top" bg='light'>
+            <Container Container >
+                <Navbar.Brand href="#home">Resep<span>ku</span></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#">Home</Nav.Link>
+                        <Nav.Link href="#">Categories</Nav.Link>
+                        <Nav.Link href="#">Products</Nav.Link>
+                    </Nav>
+                    <Nav className="ms-auto">
+                        <Button variant='light' className='mx-2 login'>Login</Button>
+                        <Button className='mx-2 signup'>Sign Up</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container >
+        </Navbar>
+    )
+}
