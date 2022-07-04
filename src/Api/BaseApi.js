752f895356661b29b872209ef0f16ac4e8282798
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-07-04 16:01:58 +0700

    create highlight card

--- src/Api/BaseApi.js
+++ src/Api/BaseApi.js
@@ -18,10 +18,25 @@ class BaseApi {
         return result
     }
 
-    static async categoriesRecipe() {
+    static async allCategoriesRecipe() {
         let result = null;
 
         await axios.get(BaseApi.baseUrl + 'api/category/recipes')
+            .then((response) => {
+                // console.log(response.data);
+                result = response.data
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        // console.log(result);
+        return result
+    }
+
+    static async detailRecipe(key) {
+        let result = null;
+
+        await axios.get(BaseApi.baseUrl + `api/recipe/${key}`)
             .then((response) => {
                 // console.log(response.data);
                 result = response.data
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Carousel, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 import { BaseApi } from '../Api/BaseApi';
 import { IoFastFood, IoTime, IoCellular } from 'react-icons/io5';
 
 
 export default function Home() {
     const getAllCategories = async () => {
-        const data = await BaseApi.categoriesRecipe();
+        const data = await BaseApi.allCategoriesRecipe();
         if (data.status === true) {
             setAllCategories(data.results)
         }
@@ -19,16 +19,26 @@ export default function Home() {
         }
     };
 
-    const [higlightRecipe, setHiglightRecipe] = useState([]);
+    const getRecipeRotiGoreng = async () => {
+        const data = await BaseApi.detailRecipe("resep-roti-goreng-isi-tempe-lada-hitam");
+        if (data.status === true) {
+            setDetailRecipe(data.results)
+        }
+    };
+
     const [allCategories, setAllCategories] = useState([]);
+    const [higlightRecipe, setHiglightRecipe] = useState([]);
+    const [detailRecipe, setDetailRecipe] = useState([]);
 
     useEffect(() => {
         getHiglightRecipe()
         getAllCategories()
+        getRecipeRotiGoreng()
     }, []);
 
     return (
         <>
+            {console.log(detailRecipe)}
             <Card className='header'>
                 <Card.Img src="./images/header.png" alt="Card image" />
                 <Card.ImgOverlay className='d-flex align-items-center justify-content-center text-center'>
@@ -51,40 +61,77 @@ export default function Home() {
             <Container className='mt-5'>
 
                 {/* All Categories */}
-                <Card.Title className='fw-bolder fs-4 my-4'>Berdasarkan Kategori</Card.Title>
-                <div className="mb-5 categories-card">
-                    {allCategories.map((item) => (
-                        <Card className="text-center mb-2">
-                            <Card.Img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-                            <Card.ImgOverlay className='d-flex justify-content-center align-items-center'>
-                                <Card.Title>{item.category}</Card.Title>
-                            </Card.ImgOverlay>
-                        </Card>
-                    ))}
+                <div className="categories-component">
+                    <Card.Title className='fw-bolder fs-4 my-4'>Berdasarkan Kategori</Card.Title>
+                    <div className="mb-5 categories-card">
+                        {allCategories.map((item) => (
+                            <Card className="text-center mb-2" key={item.key}>
+                                <Card.Img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                                <Card.ImgOverlay className='d-flex justify-content-center align-items-center'>
+                                    <Card.Title>{item.category}</Card.Title>
+                                </Card.ImgOverlay>
+                            </Card>
+                        ))}
+                    </div>
                 </div>
 
                 {/* New Recipes */}
-                <Card className='categories-title flex-row justify-content-between align-items-center my-4'>
-                    <Card.Title className='fw-bolder fs-4'>Cobain Resep Terbaru</Card.Title>
-                    <Button>Lihat Semua</Button>
-                </Card>
+                <div className="new-recipe-component">
+                    <Card className='categories-title flex-row justify-content-between align-items-center my-4'>
+                        <Card.Title className='fw-bolder fs-4'>Cobain Resep Terbaru</Card.Title>
+                        <Button>Lihat Semua</Button>
+                    </Card>
 
-                <Row xs={1} sm={2} md={4} className="g-4 mb-5">
-                    {higlightRecipe.map((item) => (
-                        <Col>
+                    <Row xs={1} sm={2} md={4} className="g-4 mb-5">
+                        {higlightRecipe.map((item) => (
+                            <Col key={item.key}>
+                                <Card className='new-recipe-card'>
+                                    <Card.Img variant="top" src={item.thumb} />
+                                    <Card.Body>
+                                        <Card.Title>{item.title}</Card.Title>
+                                        <Card.Text className='mt-auto'>
+                                            <IoTime /> {item.times} <IoFastFood /> {item.portion}
+                                        </Card.Text>
+                                        <Card.Text><IoCellular /> {item.dificulty}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+
+                {/* Recipe Highlight */}
+                {detailRecipe.length !== 0 ? (<Card className='highlight-recipe-component'>
+                    <Row className="m-2 g-0">
+                        <Col md={5} className='d-flex align-items-center'>
+                            <Card.Img src={detailRecipe.thumb} className="img-fluid rounded-start" alt="foto makanan" />
+                        </Col>
+                        <Col className='d-flex align-items-center ms-3'>
                             <Card className='highlight-recipe-card'>
-                                <Card.Img variant="top" src={item.thumb} />
                                 <Card.Body>
-                                    <Card.Title>{item.title}</Card.Title>
-                                    <Card.Text className='mt-auto'>
-                                        <IoTime /> {item.times} <IoFastFood /> {item.portion}
-                                    </Card.Text>
-                                    <Card.Text><IoCellular /> {item.dificulty}</Card.Text>
+                                    <Card.Title className='fw-bolder fs-5'>{detailRecipe.title}</Card.Title>
+                                    <Card.Text className='my-1'><small>{detailRecipe.author.datePublished} oleh {detailRecipe.author.user}</small></Card.Text>
+                                    <Card.Text>{detailRecipe.desc}</Card.Text>
+                                    <Button>Cek Detail</Button>
                                 </Card.Body>
                             </Card>
                         </Col>
-                    ))}
-                </Row>
+                    </Row>
+                </Card>) : ("")}
+
+                {/* newsletter */}
+                <Card className="text-center my-5 newsletter">
+                    <Card.Body>
+                        <Card.Title className='my-3 mx-3'>Bergabunglah dengan Buletin Kami <br /> Terima Update Resep & Artikel Masakan Terbaru</Card.Title>
+                        <Form className="d-flex justify-content-center mb-4 mx-3">
+                            <Form.Control
+                                type="email"
+                                placeholder="Email anda"
+                            />
+                            <Button>Kirim</Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
             </Container>
         </>
     )
