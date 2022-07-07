568851a35ea228a8b95ae7c8a51c6eedb9339cb0
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-07-07 19:01:20 +0700

    update if page not found

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Button, Card, Container } from 'react-bootstrap'
 
 export default function NotFound() {
     return (
-        <div>NotFound</div>
+        <Container className='page-not-found text-center my-5'>
+            <Card>
+                <Card.Img src="./images/notfound.png" alt="Card image" />
+                <Card.Body>
+                    <Card.Title className='fw-bold fs-1'>Ooops...</Card.Title>
+                    <Card.Text>
+                        Halaman belum tersedia untuk saat ini. Klik tombol dibawah untuk melanjutkan perjalanan kembali.
+                    </Card.Text>
+                    <a href='/' className='btn btn-primary'>Halaman Utama</a>
+                </Card.Body>
+            </Card>
+        </Container>
     )
 }
