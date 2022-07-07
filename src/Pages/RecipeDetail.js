56bae9fe01c99d7caabfabab003e52519498ccdb
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-07-07 17:55:43 +0700

    update ui recipe detail

--- src/Pages/RecipeDetail.js
+++ src/Pages/RecipeDetail.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { BaseApi } from '../Api/BaseApi';
 
@@ -19,10 +20,46 @@ export default function RecipeDetail() {
     }, []);
 
     return (
-        <>
+        <div className='recipe-detail'>
             {console.log(recipeDetail)}
-            <div>RecipeDetail</div>
-            <h1>{recipeDetail.title}</h1>
-        </>
+            <Container className='mt-5'>
+                <img className='header-thumbnail' src={recipeDetail.thumb} alt="Logo" />
+                <Card className='text-center card-content my-5 justify-content-center'>
+                    <Card.Title className='fw-bold fs-3'>{recipeDetail.title}</Card.Title>
+                    <div className='card-info my-3 d-flex justify-content-center shadow'>
+                        <div className='box'>
+                            <small>Porsi</small>
+                            <Card.Text>{recipeDetail.servings}</Card.Text>
+                        </div>
+                        <div className='box'>
+                            <small>Waktu</small>
+                            <Card.Text>{recipeDetail.times}</Card.Text>
+                        </div>
+                        <div className='box'>
+                            <small>Tingkat Kesusahan</small>
+                            <Card.Text>{recipeDetail.dificulty}</Card.Text>
+                        </div>
+                    </div>
+                    <div className='card-ingredient'>
+                        <div>
+                            <Card.Title className='my-4 fw-bold'>Bahan yang Diperlukan</Card.Title>
+                            <Row xs={1} md={2} className="g-2 mx-5">
+                                {recipeDetail.ingredient.map(item => (
+                                    <Col>
+                                        <Card.Text>{item}</Card.Text>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </div>
+                        <div className='mx-5'>
+                            <Card.Title className='my-4 fw-bold'>Cara Memasak</Card.Title>
+                            {recipeDetail.step.map(item => (
+                                <Card.Text>{item}</Card.Text>
+                            ))}
+                        </div>
+                    </div>
+                </Card>
+            </Container>
+        </div>
     )
 }
