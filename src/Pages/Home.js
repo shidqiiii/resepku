ab5dc06f0f62f4601b08ac045bfd2a577a484276
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-07-07 16:06:41 +0700

    update ui

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -89,15 +89,34 @@ export default function Home() {
                                 <Row xs={1} sm={2} md={4} className="g-4 mb-5">
                                     {higlightRecipe.map((item) => (
                                         <Col key={item.key}>
-                                            <Card className='new-recipe-card'
+                                            <Card className='new-recipe-card text-center'
                                                 onClick={() => navigateToRecipeDetail(item.key)}>
                                                 <Card.Img variant="top" src={item.thumb} />
                                                 <Card.Body>
-                                                    <Card.Title>{item.title}</Card.Title>
-                                                    <Card.Text className='mt-auto'>
-                                                        <IoTime /> {item.times} <IoFastFood /> {item.portion}
-                                                    </Card.Text>
-                                                    <Card.Text><IoCellular /> {item.dificulty}</Card.Text>
+                                                    <Card.Title className='fw-bold'>{item.title}</Card.Title>
+                                                    <hr className='my-2' />
+                                                    <Row>
+                                                        <Col>
+                                                            <small><IoTime /></small>
+                                                        </Col>
+                                                        <Col>
+                                                            <small><IoFastFood /></small>
+                                                        </Col>
+                                                        <Col>
+                                                            <small><IoCellular /></small>
+                                                        </Col>
+                                                    </Row>
+                                                    <Row>
+                                                        <Col>
+                                                            <Card.Text>{item.times}</Card.Text>
+                                                        </Col>
+                                                        <Col>
+                                                            <Card.Text>{item.portion}</Card.Text>
+                                                        </Col>
+                                                        <Col>
+                                                            <Card.Text>{item.dificulty}</Card.Text>
+                                                        </Col>
+                                                    </Row>
                                                 </Card.Body>
                                             </Card>
                                         </Col>
